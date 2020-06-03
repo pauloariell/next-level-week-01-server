1874a034654494ff5d019b064f389b9565259760
PAC <PAC@NBPAC>
2020-06-03 01:11:33 -0300

    Fazendo insert no banco de dados

--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,11 @@ import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (req,res) =>{
-  return res.json({message:'Ta funfando!'})
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Ta funfando!' })
 });
 
-routes.get('/items', async(req, res) => {
+routes.get('/items', async (req, res) => {
   const items = await knex('items').select('*');
 
   const serializedItems = items.map(item => {
@@ -18,6 +18,41 @@ routes.get('/items', async(req, res) => {
   })
 
   return res.json(serializedItems);
+});
+
+routes.post('/points', async (req, res) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = req.body;
+  
+  const ids = await knex('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf
+  });
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id: ids[0]
+    }
+  });
+
+  await knex('point_items').insert(pointItems);
+
+  return res.json({sucess:true});
 });
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,9 @@ import routes from './routes';
 import path from 'path';
 
 const app = express();
+app.use(express.json());
 app.use(routes);
 
 app.use( '/uploads', express.static( path.resolve( __dirname, '..', 'uploads') ) );
 
-app.listen(3333);
+app.listen(33333);
