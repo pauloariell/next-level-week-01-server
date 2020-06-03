9190cec4c9e0816900d1d2446c8eaf4579ff1b7a
PAC <PAC@NBPAC>
2020-06-02 23:36:47 -0300

    Inicial Server

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express'; //when have triple dot, that is, because don't have type for this import;
+import routes from './routes';
+import path from 'path';
+
+const app = express();
+app.use(routes);
+
+app.use( '/uploads', express.static( path.resolve( __dirname, '..', 'uploads') ) );
+
+app.listen(3333);
